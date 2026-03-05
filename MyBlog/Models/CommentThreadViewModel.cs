2fe9d5e02c06b51106f53ed1b13f2c9e50b97f73
Mark Golley <mark.golley@icloud.com>
2026-03-05 22:03:46 +0000

    Add replies feature

---
+++ MyBlog/Models/CommentThreadViewModel.cs
@@ -0,0 +1,10 @@
+namespace MyBlog.Models;
+
+public class CommentThreadViewModel
+{
+    public required Comment Comment { get; init; }
+    public int Depth { get; init; }
+    public int TotalReplyCount { get; init; }
+    public string ContentHtml { get; init; } = string.Empty;
+    public List<CommentThreadViewModel> Replies { get; init; } = new();
+}
